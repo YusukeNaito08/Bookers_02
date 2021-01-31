83179b943341aa4436d1f74e94190e97e9645f64
Yusukenaito08 <yusuke0208na@gmail.com>
2021-01-31 02:49:56 +0000

    [イイね機能 done]

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,5 +1,5 @@
 class BookComment < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  
+  has_many :favorites, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only:[:show,:index,:edit,:update]
   root to:  "homes#top"
   resources :books, only:[:index,:show,:create,:edit,:update,:destroy] do
+    resource :favorites, only:[:create,:destroy]
   resources :book_comments, only:[:create, :destroy]
  end
 end
